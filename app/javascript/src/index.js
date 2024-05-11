12a312b13f585abee08bc3357c36242e27952551
CodeFlow0 <jacoboquinn4@gmail.com>
2024-05-11 12:40:49 -0500

    Added todo list frontend

--- app/javascript/src/index.js
+++ app/javascript/src/index.js
@@ -7,9 +7,15 @@ import {
 
 indexTasks(function (response) {
   var htmlString = response.tasks.map(function(task) {
-    return "<div class='col-12 mb-3 p-2 border rounded task' data-id='" + task.id + "'> \
-      " + task.content + "\
-      </div>";
+    return `<div class='col-12 mb-3 p-2 border rounded d-flex align-items-center justify-content-between task' data-id='${task.id}'> 
+      <div class="d-flex align-items-center">
+        ${task.content}
+      </div>
+      <div>
+        <button class="btn btn-danger">Delete</button>
+        <input type="checkbox" id="task-${task.id}" />
+      </div>
+    </div>`;
   });
 
   $("#tasks").html(htmlString);
